1dd60caae622ea4505936355a17e11a73c206f2b
Tim Hutt <timothy.hutt@codasip.com>
2025-02-26 09:40:13 +0000

    Remove tutorial comment

--- test/sail-riscv-zig-tests/build.zig
+++ test/sail-riscv-zig-tests/build.zig
@@ -37,10 +37,6 @@ pub fn build(b: *std.Build) void {
 
     // Module for runtime support (crt0.S etc).
     const runtime_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/runtime.zig"),
         .target = target,
         .optimize = optimize,
